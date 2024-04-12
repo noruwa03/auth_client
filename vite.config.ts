7bfee785973bf990296d234dcfa98c54096ccc74
noruwa03 <noruwaobaseki@gmail.com>
2024-04-12 20:09:57 +0100

    eightheen

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   server: {
     proxy: {
-      "/api/v1": {
+      "/api": {
         target: "http://localhost:8000",
         secure: false,
       },
